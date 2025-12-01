13a3ba4dcbbe3ecd3fb52dab21004667bcff6d96
Younes193 <younesbouk09@gmail.com>
2025-12-01 15:07:43 +0100

    projet

--- src/test_python.py
+++ src/test_python.py
@@ -1,25 +1,30 @@
 print("hello")
 import csv
+import requests
+import pandas
+import folium
 with open('src/liste.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
         print(', '.join(row))
-import folium
+
+#_______________________________________________________________________________________
+
 l=folium.Map(location=(43.2898668,5.3834864),zoom_start=5)
 folium.Marker(
     location=[48.8588255,2.2646331],
-    tooltip="Click me!",
-    popup="Mt. Hood Meadows",
+    tooltip="Paris!",
+    popup="paname",
     icon=folium.Icon(icon="cloud"),
 ).add_to(l)
 
 folium.Marker(
     location=[43.292614, 5.366633],
-    tooltip="Click me!",
-    popup="Timberline Lodge",
-    icon=folium.Icon(color="green"),
+    tooltip="Marseille",
+    popup="la ville bleue",
+    icon=folium.Icon(color="noir"),
 ).add_to(l)
-m = folium.Map(location=[-71.38, -73.9], zoom_start=11)
+#___________________________________________________________
 
 trail_coordinates = [
     (43.2802207,5.2158271),
@@ -28,5 +33,46 @@ trail_coordinates = [
     (47.2382037,-1.6427356),
     (48.8588255,2.2646331),
 ]
+
+# __________________________________________________________________
+
 folium.PolyLine(trail_coordinates, tooltip="Coast").add_to(l)
+
+ls = folium.PolyLine(
+    locations=[[36.75, 3.06], [36.75, 5.07], [35.39, 5.37], [35.3507, 3.3641], [36.75, 3.06]], color="red"
+)
+
+ls.add_child(folium.Popup("outline Popup on Polyline"))
+ls.add_to(l)
+
+gj = folium.GeoJson(
+    data={"type": "Polygon", "coordinates": [[[27, 43], [33, 43], [33, 47], [27, 47]]]}
+)
+
+gj.add_child(folium.Popup("outline Popup on GeoJSON"))
+gj.add_to(l)
+#_______________________________________________________________________
+
+
+state_geo = requests.get(
+    "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_states.json"
+).json()
+state_data = pandas.read_csv(
+    "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_unemployment_oct_2012.csv"
+)
+
+
+folium.Choropleth(
+    geo_data=state_geo,
+    name="choropleth",
+    data=state_data,
+    columns=["State", "Unemployment"],
+    key_on="feature.id",
+    fill_color="YlGn",
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name="Unemployment Rate (%)",
+).add_to(l)
+
+folium.LayerControl().add_to(l)
 l.save("montre.html")
