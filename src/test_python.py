c528661b6f81467aa23f5bf30a9b61ce27ce6060
Younes193 <younesbouk09@gmail.com>
2025-12-02 09:24:20 +0100

    script

--- src/test_python.py
+++ src/test_python.py
@@ -61,28 +61,5 @@ gj = folium.GeoJson(
 
 gj.add_child(folium.Popup("outline Popup on GeoJSON"))
 gj.add_to(l)
-#_______________________________________________________________________
-
-
-state_geo = requests.get(
-    "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_states.json"
-).json()
-state_data = pandas.read_csv(
-    "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_unemployment_oct_2012.csv"
-)
-
-
-folium.Choropleth(
-    geo_data=state_geo,
-    name="choropleth",
-    data=state_data,
-    columns=["State", "Unemployment"],
-    key_on="feature.id",
-    fill_color="YlGn",
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name="Unemployment Rate (%)",
-).add_to(l)
-
-folium.LayerControl().add_to(l)
 l.save("montre.html")
+#_______________________________________________________________________
