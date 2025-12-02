449c730bb05863da1b7254787c95bc7cbf73ac2d
Younes193 <younesbouk09@gmail.com>
2025-12-02 18:55:44 +0100

    teste 2

--- src/test_python.py
+++ src/test_python.py
@@ -6,26 +6,38 @@ import folium
 import folium
 
 # Coordonnées de l'endroit où placer le marqueur
-latitude = 43.2850044
-longitude = 5.3908346
+latitude = 43.28498595454043 
+longitude = 5.393399663866488
 
 # Informations à afficher
 telephone = "+33 4 86 11 09 26"
 email = "contact@example.com"
+jours_ouverts = """
 
-# Contenu HTML du popup
+Lundi : 13;30h - 15;30h<br>,
+Mardi : Fermé,
+Mercredi : 9h - 11h<br>,
+Jeudi : Fermé,
+Vendredi : 9h - 11h<br>,
+Samedi : Fermé,
+Dimanche : Fermé
+"""
+# Contenu HTML de la popup
 popup_html = f"""
 <b>Contact :</b><br>
 📞 Téléphone : {telephone}<br>
-✉️ Email : <a href="mailto:{email}">{email}</a>
+✉️ Email : <a href="mailto:{email}">{email}</a><br><br>
+<b>Jours d'ouverture :</b><br>
+{jours_ouverts}
 """
 
+
 # Création de la carte centrée sur le monde
-carte = folium.Map(location=[20, 0], zoom_start=2)
+carte = folium.Map(location=[46.5, 2.5], zoom_start=5)
 
 # Ajout du marqueur
 folium.Marker(
-    location=[latitude, longitude],
+    location=[43.28498595454043, 5.393399663866488],
     popup=folium.Popup(popup_html, max_width=300),
     tooltip="Médecins du Monde"
 ).add_to(carte)
